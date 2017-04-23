c3981499a7fa9189eb070c52613ec02d32c2b893
Thijs Damsma <thijs.damsma@vanoord.com>
2017-04-23 21:40:59 +0200

    merge all code to __init__.py

--- examples/flask_tut/flask_tut/__init__.py
+++ examples/flask_tut/flask_tut/__init__.py
@@ -3,12 +3,53 @@ from flask import render_template, request, jsonify
 
 from datatables import ColumnDT, DataTables
 
-from flask_tut.models import (
-    app,
-    db,
-    User,
-    Address,
-)
+import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_pyfile('../app.cfg')
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+
+    """Define a User."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode, unique=True)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    address = db.relationship(
+        'Address', uselist=False, backref=db.backref('user'))
+
+    def __unicode__(self):
+        """Give a readable representation of an instance."""
+        return '%s' % self.name
+
+    def __repr__(self):
+        """Give a unambiguous representation of an instance."""
+        return '<%s#%s>' % (self.__class__.__name__, self.id)
+
+
+class Address(db.Model):
+
+    """Define an Address."""
+
+    __tablename__ = 'addresses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.Unicode, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __unicode__(self):
+        """Give a readable representation of an instance."""
+        return '%s' % (self.id)
+
+    def __repr__(self):
+        """Give a unambiguous representation of an instance."""
+        return '<%s#%s>' % (self.__class__.__name__, self.id)
 
 
 @app.route("/")
